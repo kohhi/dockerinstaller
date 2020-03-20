c7b92f8ebb88ebb0e5808d52115ba43f02252df7
nakochi <miho@kohhi.net>
2020-03-20 10:05:11 +0900

    add RHEL

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Fedora:
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/kohhi/dockerinstaller/master/installer-fedora.sh)"
 ```
 
+RedHat:
+
+```shell
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/kohhi/dockerinstaller/master/installer-rhel.sh)"
+```
--- installer-rhel.sh
+++ installer-rhel.sh
@@ -7,13 +7,18 @@ if ! ${PASSWORD+:} false; then
   read -s PASSWORD
 fi
 
+###### Install dnf
+if [ "$(cat /etc/redhat-release | grep -o [0-9]\.[0-9] | tr -d .)" -lt "80" ]; then
+  echo "${PASSWORD}" | sudo -S yum -y install dnf
+fi
+
 ###### Install Docker
 echo "Uninstalling Old Docker Package..."
 echo "${PASSWORD}" | sudo -S dnf -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine docker-ce docker-ce-cli containerd.io
 echo "Installing dnf-plugins-core..."
 echo "${PASSWORD}" | sudo -S dnf -y install dnf-plugins-core
 echo "Adding Docker Repository..."
-echo "${PASSWORD}" | sudo -S dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+echo "${PASSWORD}" | sudo -S dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 echo "Installing Docker..."
 echo "${PASSWORD}" | sudo -S dnf -y install docker-ce docker-ce-cli containerd.io
 
@@ -21,11 +26,6 @@ echo "${PASSWORD}" | sudo -S dnf -y install docker-ce docker-ce-cli containerd.i
 echo "Installing docker-compose..."
 echo "${PASSWORD}" | sudo -S curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo "${PASSWORD}" | sudo -S chmod +x /usr/local/bin/docker-compose
-
-###### For fedora 31 (or above)
-if [ "$(cat /etc/fedora-release | grep -o [0-9].)" -ge "31" ]; then
-  echo "${PASSWORD}" | sudo -S grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
-fi
 
 ###### Start Docker
 echo "Starting Docker..."
